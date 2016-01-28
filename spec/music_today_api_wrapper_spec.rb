b110a8694a47bddbf5dc2522692cf57c8a0c3351
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2016-01-28 13:48:08 -0300

    Support size as required param

--- spec/music_today_api_wrapper_spec.rb
+++ spec/music_today_api_wrapper_spec.rb
@@ -10,12 +10,39 @@ describe 'test the entire gem' do
       response = MusicTodayApiWrapper.products
       expect(response.class).to eq(MusicTodayApiWrapper::RestClients::CommonResponse)
       expect(response.success?).to eq(true)
+      expect(response.data[:products].length).to eq(3)
+    end
+  end
+
+  it 'should return the products list with the right name data' do
+    VCR.use_cassette("get_products") do
+      response = MusicTodayApiWrapper.products
       first_product = response.data[:products][0]
-      expect(response.data[:products].length).to eq(9)
-      expect(first_product.class).to eq(MusicTodayApiWrapper::Resources::Product)
-      expect(first_product.image.class).to eq(MusicTodayApiWrapper::Resources::Image)
-      expect(first_product.name).to eq('DMB Live Trax Vol. 35: Post-Gazette Pavilion')
-      expect(first_product.image.short).to eq('http://static.musictoday.com/store/bands/93/product_small/POTSGAZETTE.JPG')
+      expect(first_product.name).to eq('Amazing T-Shirt')
+    end
+  end
+
+  it 'should return the products list with the right images data small case' do
+    VCR.use_cassette("get_products") do
+      response = MusicTodayApiWrapper.products
+      first_product = response.data[:products][0]
+      expect(first_product.image.short).to eq('http://static.musictoday.com/store/bands/12/product_small/AAA001.JPG')
+    end
+  end
+
+  it 'should return the products list with the right images data medium case' do
+    VCR.use_cassette("get_products") do
+      response = MusicTodayApiWrapper.products
+      first_product = response.data[:products][0]
+      expect(first_product.image.medium).to eq('http://static.musictoday.com/store/bands/12/product_medium/AAA001.JPG')
+    end
+  end
+
+  it 'should return the products list with the right images data large case' do
+    VCR.use_cassette("get_products") do
+      response = MusicTodayApiWrapper.products
+      first_product = response.data[:products][0]
+      expect(first_product.image.medium).to eq('http://static.musictoday.com/store/bands/12/product_medium/AAA001.JPG')
     end
   end
 
@@ -26,85 +53,118 @@ describe 'test the entire gem' do
       expect(response.class).to eq(MusicTodayApiWrapper::RestClients::CommonResponse)
       expect(response.success?).to eq(true)
       expect(response.data[:products].length).to eq(2)
-
-      first_product = response.data[:products][0]
-      second_product = response.data[:products][1]
-
-      expect(first_product.class).to eq(MusicTodayApiWrapper::Resources::Product)
-      expect(first_product.uid).to eq('DM396COMBO')
-      expect(first_product.class).to eq(MusicTodayApiWrapper::Resources::Product)
-      expect(first_product.image.class).to eq(MusicTodayApiWrapper::Resources::Image)
-      expect(first_product.name).to eq("Live Trax Vol. 35 Women's T-shirt Bundle")
-      expect(first_product.image.short).to eq('http://static.musictoday.com/store/bands/93/product_small/DM396COMBO.JPG')
-
-      expect(second_product.class).to eq(MusicTodayApiWrapper::Resources::Product)
-      expect(second_product.uid).to eq('DM397COMBO')
-      expect(second_product.class).to eq(MusicTodayApiWrapper::Resources::Product)
-      expect(second_product.image.class).to eq(MusicTodayApiWrapper::Resources::Image)
-      expect(second_product.name).to eq('DMB Live Trax Vol. 35 + Tee + Metal Sign Bundle')
-      expect(second_product.image.short).to eq('http://static.musictoday.com/store/bands/93/product_small/DM397COMBO.JPG')
     end
   end
 
-  it 'should return the right variant' do
+  it 'should return return the right data for first product paginated' do
+    VCR.use_cassette("get_products_filtered") do
+      response = MusicTodayApiWrapper.products(2, 2)
+
+      first_product = response.data[:products][0]
+
+      expect(first_product.class).to eq(MusicTodayApiWrapper::Resources::Product)
+      expect(first_product.uid).to eq('AAA002')
+      expect(first_product.class).to eq(MusicTodayApiWrapper::Resources::Product)
+      expect(first_product.image.class).to eq(MusicTodayApiWrapper::Resources::Image)
+    end
+  end
+
+  it 'should return return the right data for second product paginated' do
+    VCR.use_cassette("get_products_filtered") do
+      response = MusicTodayApiWrapper.products(2, 2)
+
+      second_product = response.data[:products][1]
+
+      expect(second_product.class).to eq(MusicTodayApiWrapper::Resources::Product)
+      expect(second_product.uid).to eq('AAA003')
+      expect(second_product.class).to eq(MusicTodayApiWrapper::Resources::Product)
+      expect(second_product.image.class).to eq(MusicTodayApiWrapper::Resources::Image)
+    end
+  end
+
+  it 'should return the right variant sku' do
     VCR.use_cassette("get_products") do
       response = MusicTodayApiWrapper.products
       first_product = response.data[:products][0]
       first_variant = first_product.variants.first
 
       expect(first_product.variants.class).to eq(Array)
-      expect(first_variant.sku).to eq('DMCD123')
-      expect(first_variant.price).to eq(15.99)
-      expect(first_variant.quantity_available).to eq(99999)
+      expect(first_variant.sku).to eq('AAA001SLBK')
+    end
+  end
+
+  it 'should return the right variant price' do
+    VCR.use_cassette("get_products") do
+      response = MusicTodayApiWrapper.products
+      first_product = response.data[:products][0]
+      first_variant = first_product.variants.first
+      expect(first_variant.price).to eq(24.99)
+    end
+  end
+
+  it 'should return the right variant amount' do
+    VCR.use_cassette("get_products") do
+      response = MusicTodayApiWrapper.products
+      first_product = response.data[:products][0]
+      first_variant = first_product.variants.first
+      expect(first_variant.quantity_available).to eq(1299)
     end
   end
 
   it 'should return a product data' do
     VCR.use_cassette("get_product") do
-      response = MusicTodayApiWrapper.find_product('DMDD132')
+      response = MusicTodayApiWrapper.find_product('AAA001')
       expect(response.class).to eq(MusicTodayApiWrapper::RestClients::CommonResponse)
       expect(response.success?).to eq(true)
       expect(response.data[:product].class).to eq(MusicTodayApiWrapper::Resources::Product)
       expect(response.data[:product].image.class).to eq(MusicTodayApiWrapper::Resources::Image)
-      expect(response.data[:product].name).to eq('DMB Live Trax Vol. 35: Post-Gazette Pavilion')
-      expect(response.data[:product].image.short).to eq('http://static.musictoday.com/store/bands/93/product_small/POTSGAZETTE.JPG')
+    end
+  end
+
+  it 'should return right product name' do
+    VCR.use_cassette("get_product") do
+      response = MusicTodayApiWrapper.find_product('AAA001')
+      expect(response.data[:product].name).to eq('Amazing T-Shirt')
     end
   end
 
   it 'should return shipping options for a product' do
     VCR.use_cassette('get_shipping_options') do
       address =
-        MusicTodayApiWrapper::Resources::Address.new('2 Lexington Avenue, New York, NY',
+        MusicTodayApiWrapper::Resources::Address.new('5391 Three Notched Road',
                                                      'Crozet',
                                                      'VA',
                                                      '22932')
       variant =
-        MusicTodayApiWrapper::Resources::Variant.new('DMAM539',100, 14.5)
-
-      item = MusicTodayApiWrapper::Resources::Purchase::Item.new(variant, 2)
+        MusicTodayApiWrapper::Resources::Variant.new('AAA002SLBK', 24.99, 1)
+      item = MusicTodayApiWrapper::Resources::Purchase::Item.new(variant, 1)
       response = MusicTodayApiWrapper.shipping_options(address, [item])
-      expect(response.data[:shipping_options][0].type).to eq('EXPEDITED')
-      expect(response.data[:shipping_options][0].rate).to eq(14.11)
-      expect(response.data[:shipping_options][0].description).to eq('Expedited (3-7 business days; Not guaranteed for Christmas delivery.)')
+      expect(response.data[:shipping_options][0].type).to eq('STANDARD')
+      expect(response.data[:shipping_options][0].rate).to eq(6.29)
+      expect(response.data[:shipping_options][0].description).to eq('Standard (5-9 business days)')
     end
   end
 
   it 'should return a checkout session with right data' do
     VCR.use_cassette('post_cart_pricing') do
       address =
-        MusicTodayApiWrapper::Resources::Address.new('2 Lexington Avenue',
+        MusicTodayApiWrapper::Resources::Address.new('5391 Three Notched Road',
                                                      'Crozet',
                                                      'VA',
-                                                     '22932')
+                                                     '22932',
+                                                     'US',
+                                                     '',
+                                                     [],
+                                                     'STANDARD')
       variant =
-        MusicTodayApiWrapper::Resources::Variant.new('DMAM539',100, 14.5)
+        MusicTodayApiWrapper::Resources::Variant.new('AAA002SLBK', 24.99, 1)
 
       item = MusicTodayApiWrapper::Resources::Purchase::Item.new(variant)
       response = MusicTodayApiWrapper.checkout(address, [item])
       expect(response.class).to eq(MusicTodayApiWrapper::RestClients::CommonResponse)
       expect(response.success?).to eq(true)
       expect(response.data[:session].class).to eq(MusicTodayApiWrapper::Resources::Checkout::Session)
-      expect(response.data[:session].id).to eq('03b501ca-f0a7-4733-958a-e3402082e851')
+      expect(response.data[:session].id).to eq('FAKE-SESSION-NUMBER')
     end
   end
 end
